793d7014f6c5386b3c9b9249929349b9122a988e
trucount <trucount.entriprises@gmail.com>
2026-05-30 21:43:50 +0530

    Clear icon URLs in layout.tsx
    Removed icon URLs from layout configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,19 +49,19 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
+        url: "",
         media: "(prefers-color-scheme: light)",
       },
       {
-        url: "/icon-dark-32x32.png",
+        url: "",
         media: "(prefers-color-scheme: dark)",
       },
       {
-        url: "/icon.svg",
+        url: "",
         type: "image/svg+xml",
       },
     ],
-    apple: "/apple-icon.png",
+    apple: "",
   },
 }
 
