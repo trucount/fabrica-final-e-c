baf5c4f9be95ca156e4ac3c29d6c4cdd851d1f8e
trucount <trucount.entriprises@gmail.com>
2026-05-30 08:41:14 +0530

    Update admin and edit passwords

--- app/edit/actions.ts
+++ app/edit/actions.ts
@@ -12,7 +12,7 @@ import { parseInfoPageSlug, saveInfoPageContent, type InfoPageContent } from "@/
 import { EDIT_SESSION_COOKIE } from "./constants"
 import { requireEditPageAccess } from "./auth"
 
-const EDIT_PASSWORD = "sparrowaisoultions"
+const EDIT_PASSWORD = "sparrowaisolutions"
 
 export async function loginToHomeEdit(formData: FormData) {
   await loginToEdit(formData, "/edit")
